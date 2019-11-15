5e180bb0b6258adc5a8aba5b8d9765b2a08eedaf
yigexiaocaiji@qq.com <user@admindeMacBook-Air.local>
2019-11-15 15:21:43 +0800

    test2

--- test1/urls.py
+++ test1/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from . import apps
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/',apps.urls),
 ]
